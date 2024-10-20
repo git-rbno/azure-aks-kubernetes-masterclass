b070f279d6eaa91d64a163838f5e663afced0427
Rune <rbno@trails-consult.dk>
2024-10-20 12:34:06 +0200

    Updated changes -due to general updates of software

--- 24-Azure-AKS-Terraform/24-01-Terraform-Commands-Basics/v1-terraform-azurerm-provider/main.tf
+++ 24-Azure-AKS-Terraform/24-01-Terraform-Commands-Basics/v1-terraform-azurerm-provider/main.tf
@@ -1,12 +1,15 @@
 # Configure Azure Provider
-provider "azurerm" {
-  # Version is optional
-  # Terraform recommends to pin to a specific version of provider
-  #version = "=2.35.0"
-  #version = "~>2.35.0"
-  version = "~> 3.36.0" #recommended to use en specific version
-  features {}
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
 }
 
+provider "azurerm" {
+  features {}
+}
 
 
