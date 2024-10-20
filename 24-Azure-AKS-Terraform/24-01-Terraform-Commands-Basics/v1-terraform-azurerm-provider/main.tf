d9cb2547d973c26344b7e77a795d94354ddae147
Rune <rbno@trails-consult.dk>
2024-10-20 14:26:43 +0200

    24-01-Terraform change

--- 24-Azure-AKS-Terraform/24-01-Terraform-Commands-Basics/v1-terraform-azurerm-provider/main.tf
+++ 24-Azure-AKS-Terraform/24-01-Terraform-Commands-Basics/v1-terraform-azurerm-provider/main.tf
@@ -1,15 +1,9 @@
 # Configure Azure Provider
 terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+required_providers {
+  azurerm = {
+    source  = "hashicorp/azurerm"
+    version = "~> 3.0"  # Make sure there's a comma here between different key-value pairs if there are more items
     }
   }
 }
-
-provider "azurerm" {
-  features {}
-}
-
-
